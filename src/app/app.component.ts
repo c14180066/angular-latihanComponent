cbe8129b8116d2951a4bf5fdd89e941acc62faef
c14180066 <c14180066@john.petra.ac.id>
2020-04-22 10:05:20 +0700

    3 files modified, 8 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Route } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  constructor(private router : Router){}
+  input(){
+    this.router.navigate(['/input']);
+  }
+  list(){
+    this.router.navigate(['/list']);
+  }
+  edit(){
+    this.router.navigate(['edit']);
+  }
+  a = '';
+  b = '';
+  countArr = 0;
+  arrNamaJur = [];
+  arrDetailJur = [];
+  submit(data1, data2){
+    this.a = data1;
+    this.b = data2;
+    this.arrNamaJur.push(this.a);
+    this.arrDetailJur.push(this.b);
+    this.countArr = this.countArr + 1;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,21 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HalamanInputComponent } from './halaman-input/halaman-input.component';
+import { HalamanListComponent } from './halaman-list/halaman-list.component';
+import { HalamanEditComponent } from './halaman-edit/halaman-edit.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+const ROUTES: Routes=[
+  {path:'input', component: HalamanInputComponent},
+  {path:'list', component: HalamanListComponent},
+  {path:'edit', component: HalamanEditComponent}
+] 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule ,RouterModule.forRoot(ROUTES)],
+  declarations: [ AppComponent, HelloComponent, HalamanInputComponent, HalamanListComponent, HalamanEditComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
